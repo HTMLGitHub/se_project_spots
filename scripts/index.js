2b43f58df769d48517462e7ff6c574a6d2a331b2
HTMLGitHub <bak120405@gmail.com>
2024-10-28 07:41:06 -0400

    - fix validation error - have all cards displayed using js - save changes the main content and closes - close closes the modal without changing the main content - edit opens the modal

--- scripts/index.js
+++ scripts/index.js
@@ -1,79 +1,88 @@
-const initialCards =
-[
+const initialCards = [
   {
     name: "Val Thorens",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
-    alt: "Photo by Moritz Feldmann from pexels"
   },
 
   {
     name: "Restaurant terrace",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg",
-    alt: "Photo by Ceiline from pexels"
   },
 
   {
     name: "An outdoor cafe",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg",
-    alt: "Photo by Tubanur Dogan from pexels"
   },
 
   {
     name: "A very long bridge, over the forest and through the trees",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",
-    alt: "Photo by Maurice Laschet from pexels"
   },
 
   {
     name: "Tunnel with morning light",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg",
-    alt: "Photo by Van Anh Nguyen from pexels"
   },
 
   {
     name: "Mountain house",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
-    alt: "Photo by Moritz Feldmann from pexels"
-  }
+  },
 ];
 
-function createGallary() {
+function getCardElement(element) {
   const cardTemplate = document.querySelector("#card__template").content;
   const cardList = document.querySelector("#cards__list-id");
+  const card = cardTemplate.cloneNode(true);
+  card.querySelector(".card__content-image").src = element.link;
+  card.querySelector(".card__content-image").alt = element.name;
+  card.querySelector(".card__content-caption").textContent = element.name;
 
+  cardList.appendChild(card);
+}
+
+function getElement() {
   // Render the cards
-  initialCards.forEach(element => {
-    const card = cardTemplate.cloneNode(true);
-
-    card.querySelector(".card__content-image").src = element.link;
-    card.querySelector(".card__content-image").alt = element.alt;
-    card.querySelector(".card__content-caption").textContent = element.name;
-
-    cardList.appendChild(card);
-  })
+  initialCards.forEach((element) => {
+    getCardElement(element);
+  });
 }
 
 let editButton = document.querySelector(".profile__edit-button");
 let closeButton = document.querySelector(".modal__close-btn");
-let saveButton = document.querySelector(".modal__submit-btn");
 
 function showModal() {
   let modalContainer = document.querySelector(".modal");
   modalContainer.classList.add("modal_opened");
+
+  document.querySelector("#name").value = profileName;
+  document.querySelector("#description").value = profileVocation;
 }
 
-function closeModal () {
+function closeModal() {
   let modalContainer = document.querySelector(".modal");
   modalContainer.classList.remove("modal_opened");
 }
 
-function saveModal () {
+let profileName = document.querySelector(".profile__name").textContent;
+let profileVocation = document.querySelector(".profile__vocation").textContent;
+let profileForm = document.querySelector(".modal__form");
+
+function saveModal(evt) {
+  evt.preventDefault();
+
+  let nameInput = profileForm.querySelector("#name").value;
+  let vocationInput = profileForm.querySelector("#description").value;
+
+  document.querySelector(".profile__name").textContent = nameInput;
+  document.querySelector(".profile__vocation").textContent = vocationInput;
+
   let modalContainer = document.querySelector(".modal");
   modalContainer.classList.remove("modal_opened");
 }
 
 editButton.addEventListener("click", showModal);
-closeButton.addEventListener("click", closeModal)
-saveButton.addEventListener("click", saveModal);
+closeButton.addEventListener("click", closeModal);
+profileForm.addEventListener("submit", saveModal);
 
-createGallary();
+getElement();
